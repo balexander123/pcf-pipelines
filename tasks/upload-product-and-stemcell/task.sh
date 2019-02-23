7b33ba9ac50e9471d8955349a21cdacbc860b4d9
Barry Alexander <balexander@pivotal.io>
2019-02-22 22:08:29 -0800

    tshoot

--- tasks/upload-product-and-stemcell/task.sh
+++ tasks/upload-product-and-stemcell/task.sh
@@ -82,6 +82,7 @@ fi
 
 # Should the slug contain more than one product, pick only the first.
 FILE_PATH=`find ./pivnet-product -name *.pivotal | sort | head -1`
+echo FILE_PATH: $FILE_PATH
 om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
   --client-id "${OPSMAN_CLIENT_ID}" \
   --client-secret "${OPSMAN_CLIENT_SECRET}" \
